13c69413041085faedb24c3c2f90c1a14dbb58b4
Lukasz Ludwin <gorli@interia.pl>
2017-06-26 12:01:07 +0100

    first commit

---
+++ js/App.js
@@ -0,0 +1,32 @@
+﻿var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+	'X-Client-Id': '1920',
+	'X-Auth-Token': '36bbf09773cbd034f1d8b456c3ab2c5e'
+};
+
+$.ajaxSetup({
+	headers: myHeaders
+});
+
+$.ajax({
+	url: baseUrl + '/board',
+	method: 'GET',
+	success: function(response) {
+		setupColumns(response.columns);
+	}
+});
+
+function setupColumns(columns) {
+	columns.forEach(function (column) {
+		var col = new Column(column.id, column.name);
+		board.createColumn(col);
+		setupCards(col, column.cards);
+	});
+}
+
+function setupCards(col, cards) {
+	cards.forEach(function (card) {
+		var card = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
+		col.createCard(card);
+	})
+}
